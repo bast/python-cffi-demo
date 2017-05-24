34bcc8b3350bca9131441882c98ecc02eadcc9f6
Radovan Bast <bast@users.noreply.github.com>
2017-05-24 15:34:24 +0200

    rm old sources

--- test.py
+++ test.py
@@ -1,17 +1,5 @@
-from example import c_lib, fortran_lib
+#from example import c_lib, fortran_lib
 from pi import approximate_pi
-
-
-def test_c_code():
-    l = [1.0, 2.0, 3.0]
-    s = c_lib.c_sum(len(l), l)
-    assert s == 6.0
-
-
-def test_fortran_code():
-    l = [1.0, 2.0, 3.0]
-    d = fortran_lib.fortran_dot(len(l), l, l)
-    assert d == 14.0
 
 
 def test_pi():
