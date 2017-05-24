8432c1f5e593ed3c104b1826ea3b2e890fe6d64d
Radovan Bast <bast@users.noreply.github.com>
2017-05-24 15:45:35 +0200

    merge two files

--- test.py
+++ test.py
@@ -1,6 +1,25 @@
-from pi import approximate_pi_python
+import time
+import pi
 
 
 def test_pi():
-    pi = approximate_pi_python(num_points=1000000)
-    assert abs(pi - 3.141593) < 1.0e-3
+    result = pi.approximate_pi_python(num_points=1000000)
+    assert abs(result - 3.141593) < 1.0e-3
+
+
+def main():
+    num_points = 5000000
+
+    for (lang, function) in [('python', pi.approximate_pi_python),
+                             ('c', pi.approximate_pi_c),
+                             ('fortran', pi.approximate_pi_fortran)]:
+
+        t0 = time.clock()
+        result = function(num_points)
+        time_spent = time.clock() - t0
+
+        print('{0:7s} pi={1:.5f} time spent: {2:.3f} sec'.format(lang, result, time_spent))
+
+
+if __name__ == '__main__':
+    main()
