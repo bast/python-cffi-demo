0cde96921e28c06710b565cc4ec51f7bc252698b
Radovan Bast <bast@users.noreply.github.com>
2017-05-24 15:42:20 +0200

    rename

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-from pi import approximate_pi
+from pi import approximate_pi_python
 
 
 def test_pi():
-    pi = approximate_pi(num_points=1000000)
+    pi = approximate_pi_python(num_points=1000000)
     assert abs(pi - 3.141593) < 1.0e-3
