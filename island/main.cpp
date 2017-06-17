692773c53f4a674b96e572dfdb2ba60cf5010ae8
Radovan Bast <bast@users.noreply.github.com>
2017-06-17 21:35:14 +0200

    comment out calls for this exercise

--- island/main.cpp
+++ island/main.cpp
@@ -5,7 +5,7 @@
 int main()
 {
     printf("pi computed by c = %f\n", approximate_pi_c(1000000));
-    printf("pi computed by fortran = %f\n", approximate_pi_fortran(1000000));
+//  printf("pi computed by fortran = %f\n", approximate_pi_fortran(1000000));
 
     return 0;
 }
--- island/main.f90
+++ island/main.f90
@@ -1,19 +1,19 @@
 program main
 
-    use, intrinsic :: iso_c_binding, only: c_double, c_int
+!   use, intrinsic :: iso_c_binding, only: c_double, c_int
     use pi, only: approximate_pi_fortran
 
     implicit none
 
-    interface approximate_pi_c
-        function approximate_pi_c(num_points) bind (c)
-            import :: c_double, c_int
-            integer(c_int), value :: num_points
-            real(c_double) :: approximate_pi_c
-        end function
-    end interface
+!   interface approximate_pi_c
+!       function approximate_pi_c(num_points) bind (c)
+!           import :: c_double, c_int
+!           integer(c_int), value :: num_points
+!           real(c_double) :: approximate_pi_c
+!       end function
+!   end interface
 
     print *, "pi computed by fortran = ", approximate_pi_fortran(1000000)
-    print *, "pi computed by c = ", approximate_pi_c(1000000)
+!   print *, "pi computed by c = ", approximate_pi_c(1000000)
 
 end program
